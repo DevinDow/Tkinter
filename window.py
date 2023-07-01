90eb6474f760bafea8c538edb3d5fd8472433012
Devin Dow <DevinDow@gmail.com>
2023-07-01 06:37:52 -0700

    setting some properties of the root Window

--- window.py
+++ window.py
@@ -1,3 +1,8 @@
 from tkinter import *
-root = Tk()
+
+root = Tk() # create root widget
+root.title("Hello, World")
+root.configure(background="yellow")
+root.minsize(200, 200)
+root.geometry("300x300+50+50") # w 'x' h, +x, +y
 root.mainloop()
