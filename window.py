74ec2aeaaf4062d74e3a87e4935a5fe49d94b6a5
Devin Dow <DevinDow@gmail.com>
2023-07-01 06:41:44 -0700

    Labels added

--- window.py
+++ window.py
@@ -5,4 +5,11 @@ root.title("Hello, World")
 root.configure(background="yellow")
 root.minsize(200, 200)
 root.geometry("300x300+50+50") # w 'x' h, +x, +y
+
+# Create Label in our window
+text = Label(root, text="Nothing will work unless you do.")
+text.pack()
+text2 = Label(root, text="- Maya Angelou")
+text2.pack()
+
 root.mainloop()
